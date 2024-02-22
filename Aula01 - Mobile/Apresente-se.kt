90d068e86c02458d2ab2737d331198926d6e710d
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-02-22 20:24:44 -0300

    Commit Teste

---
+++ Aula01 - Mobile/Apresente-se.kt
@@ -0,0 +1,15 @@
+//Declarando variáveis em Kotlin
+
+fun main(){
+    //Declaração das variáveis
+
+    var nome = "Guilherme"
+    var idade = 24
+    var altura = 1.78
+    var inicial = "T"
+
+    //Impressão + interpolação de string
+
+    println("Meu nome é ${nome}, a inicial do meu sobrenome é ${inicial}, tenho ${idade} anos. Minha altura é ${altura}")
+
+}
