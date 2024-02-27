ba42a96ff7019253f67b5b93831732ca9cb94130
Guilherme Tantulli <130513021+GuilhermeTantulli@users.noreply.github.com>
2024-02-27 15:29:49 -0300

    Correção: Alteração de nome das Variáveis

--- DesafioVariveis/Desafio.kt
+++ DesafioVariveis/Desafio.kt
@@ -2,9 +2,9 @@
 
 fun main(){
 
-    var nota01: Double = 10.0
-    var nota02: Double = 5.0
-    var nota03: Double = 9.0
+    var nota1: Double = 10.0
+    var nota2: Double = 5.0
+    var nota3: Double = 9.0
 
     var mediaFinal: Double = (nota01 + nota02 + nota03) / 3
 
