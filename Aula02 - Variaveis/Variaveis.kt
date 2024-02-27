998cbf45bf66a92ce1a08cb03c06d95fd4a62d74
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-02-26 22:00:26 -0300

    Aula02 - parte 2

--- Aula02 - Variaveis/Variaveis.kt
+++ Aula02 - Variaveis/Variaveis.kt
@@ -6,17 +6,31 @@
 //     println("Nome: $nome, Idade: $idade")
 // }
 
+// fun main() {
+//     val numeroInteiro: Int = 10 
+//     var numeroDecimal: Double = 3.14
+//     val texto: String = "Olá, mundo!"
+//     var status: Boolean = true //se true o status é on-line se falso o status é off-line (0 ou 1)
+
+//     println("Inteiro: $numeroInteiro")
+//     println("Decimal: $numeroDecimal")
+//     println("Texto: $texto")
+//     println("Status: $status")
+
+// }
+
 fun main() {
-    val numeroInteiro: Int = 10 
-    var numeroDecimal: Double = 3.14
-    val texto: String = "Olá, mundo!"
-    var status: Boolean = true //se true o status é on-line se falso o status é off-line (0 ou 1)
+    val numeroInteiro: Int = 10
+    val numeroLong: Long = 100000000
+    val numeroDecimal: Double = 211.14
+    val caractere: Char = 'V'
+    val texto: String = "Kotlin também serve para back end"
+    val verdadeiro: Boolean = true
 
     println("Inteiro: $numeroInteiro")
+    println("Long: $numeroLong")
     println("Decimal: $numeroDecimal")
+    println("Caractere: $caractere")
     println("Texto: $texto")
-    println("Status: $status")
-
-    
-
+    println("Boolean: $verdadeiro")
 }
