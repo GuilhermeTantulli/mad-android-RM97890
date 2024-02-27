94a3c4ac511b9f39b8dab4ecd052fd74fbae8c5d
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-02-26 21:49:04 -0300

    Update - Added 'Aula02 - Variaveis'

---
+++ Aula02 - Variaveis/Variaveis.kt
@@ -0,0 +1,22 @@
+// Exemplo de variável em Kotlin
+
+// fun main() {
+//     val nome = "Guilherme" //variavel tipo string -- pode ser alterada (mutável)
+//     var idade = 25 //variável tipo int         -- não pode ser alterada (não mutável)
+//     println("Nome: $nome, Idade: $idade")
+// }
+
+fun main() {
+    val numeroInteiro: Int = 10 
+    var numeroDecimal: Double = 3.14
+    val texto: String = "Olá, mundo!"
+    var status: Boolean = true //se true o status é on-line se falso o status é off-line (0 ou 1)
+
+    println("Inteiro: $numeroInteiro")
+    println("Decimal: $numeroDecimal")
+    println("Texto: $texto")
+    println("Status: $status")
+
+    
+
+}
