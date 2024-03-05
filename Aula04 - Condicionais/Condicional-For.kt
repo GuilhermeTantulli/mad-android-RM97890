d58b0ca4c03e3a69b8f9234c512a67b9816c86a7
Guilherme Tantulli <guilherme.tantulli@gmail.com>
2024-03-04 22:42:12 -0300

    Updated: Adicionada Condicional - For

---
+++ Aula04 - Condicionais/Condicional-For.kt
@@ -0,0 +1,40 @@
+// fun main () {
+//     // Exemplo de uso - For
+
+//     for (i in 1..20) {
+//         println(i)
+//     }
+// }
+
+// fun main () {
+//     // Print com step
+
+//     for (i in 0..50 step 5) {
+//         println(i)
+//     }
+// }
+
+// fun main () {
+//     // Calcular números naturais
+
+//     val n = 10 
+//     var soma = 0
+
+
+//     for (i in 1..n) {
+//         soma += i
+//     }
+
+//     println("A soma dos primeiros $n números naturais é: $soma")
+
+// }
+
+fun main () {
+    // Caracter Separadamente
+
+    val word = "Guilherme Tantulli"
+
+    for (char in word) {
+        println(char)
+    }
+}
--- Aula04 - Condicionais/Condicional-When.kt
+++ Aula04 - Condicionais/Condicional-When.kt
@@ -1,17 +1,17 @@
-// fun main () {
+fun main () {
 
-//     // Exemplo de uso - When
+    // Exemplo de uso - When
 
-//     println("Qual menu você quer acessar: 1, 2, 3 ou 4?")
-//     val  opcao = readLine()?.toIntOrNull()
+    println("Qual menu você quer acessar: 1, 2, 3 ou 4?")
+    val  opcao = readLine()?.toIntOrNull()
 
-//     when (opcao) {
+    when (opcao) {
 
-//         1 -> println("Você escolheu o menu 1 - iPhone")
-//         1 -> println("Você escolheu o menu 2 - iPad")
-//         1 -> println("Você escolheu o menu 3 - Mac")
-//         1 -> println("Você escolheu o menu 4 - iMac")
-//         else -> println("Opção inválida! Tente novamente.")
-//     }
-// }
+        1 -> println("Você escolheu o menu 1 - iPhone")
+        1 -> println("Você escolheu o menu 2 - iPad")
+        1 -> println("Você escolheu o menu 3 - Mac")
+        1 -> println("Você escolheu o menu 4 - iMac")
+        else -> println("Opção inválida! Tente novamente.")
+    }
+}
 
