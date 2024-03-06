420171be3ed46de2f49f8fe805beb083c79e1d14
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-06 20:42:12 -0300

    Updated: Melhoria na escrita do código

--- DesafioCondicionais/IMC.kt
+++ DesafioCondicionais/IMC.kt
@@ -7,24 +7,44 @@ fun coletarDados() {
     val peso = pesoInput?.toDoubleOrNull()
     val altura = alturaInput?.toDoubleOrNull()
 
-    if (peso == null || altura == null) {
+    // if (peso == null || altura == null) {
+    //     println("Entrada inválida! Digite apenas números.")
+    //     return
+    // } else {
+    //     var imc = peso / (altura * altura)
+    //     if (imc < 18.5) {
+    //         println("Seu imc é: %.2f.".format(imc) + " Isso indica que você está com excesso de magreza.")
+    //     } else if (18.5 <= imc && imc < 25) {
+    //         println("Seu imc é: %.2f.".format(imc) + " Isso indica que seu peso está normal.")
+    //     } else if (25 <= imc && imc < 30) {
+    //         println("Seu imc é: %.2f.".format(imc) + " Isso indica que você está com excesso de peso.")
+    //     } else if (30 <= imc && imc < 35) {
+    //         println("Seu imc é: %.2f.".format(imc) + " Isso indica que você tem obesidade grau I.")
+    //     } else if (35 <= imc && imc < 40) {
+    //         println("Seu imc é: %.2f.".format(imc) + " Isso indica que você tem obesidade grau II.")
+    //     } else if (imc >= 40 ) {
+    //         println("Seu imc é: %.2f.".format(imc) + " Isso indica que você tem obesidade grau III.")
+    //     }
+
+        if (peso == null || altura == null) {
         println("Entrada inválida! Digite apenas números.")
         return
     } else {
         var imc = peso / (altura * altura)
         if (imc < 18.5) {
-            println("Seu imc é: %.2f.".format(imc) + " Isso indica que você está com excesso de magreza.")
-        } else if (18.5 <= imc && imc < 25) {
-            println("Seu imc é: %.2f.".format(imc) + " Isso indica que seu peso está normal.")
-        } else if (25 <= imc && imc < 30) {
-            println("Seu imc é: %.2f.".format(imc) + " Isso indica que você está com excesso de peso.")
-        } else if (30 <= imc && imc < 35) {
-            println("Seu imc é: %.2f.".format(imc) + " Isso indica que você tem obesidade grau I.")
-        } else if (35 <= imc && imc < 40) {
-            println("Seu imc é: %.2f.".format(imc) + " Isso indica que você tem obesidade grau II.")
-        } else if (imc >= 40 ) {
-            println("Seu imc é: %.2f.".format(imc) + " Isso indica que você tem obesidade grau III.")
+            println("Seu imc é: %.2f. Isso indica que você está com excesso de magreza.".format(imc))
+        } else if (imc < 25) {
+            println("Seu imc é: %.2f. Isso indica que seu peso está normal.".format(imc))
+        } else if (imc < 30) {
+            println("Seu imc é: %.2f. Isso indica que você está com excesso de peso.".format(imc))
+        } else if (imc < 35) {
+            println("Seu imc é: %.2f. Isso indica que você tem obesidade grau I.".format(imc))
+        } else if (imc < 40) {
+            println("Seu imc é: %.2f. Isso indica que você tem obesidade grau II.".format(imc))
+        } else if (imc >= 40) {
+            println("Seu imc é: %.2f. Isso indica que você tem obesidade grau III.".format(imc))
         }
+
     }
 }
 
