0f86baff39a98d12bd49823e2cec4c28b8169f81
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-06 19:02:11 -0300

    Updated: Nome do desafio de variáveis corrigido & Added: Desafio de condicionais - IMC

---
+++ DesafioVariveis/DesafioMedia.kt
@@ -0,0 +1,12 @@
+// Desafio 01: 
+
+fun main(){
+
+    var nota1: Double = 10.0
+    var nota2: Double = 5.0
+    var nota3: Double = 9.0
+
+    var mediaFinal: Double = (nota1 + nota2 + nota3) / 3
+
+    println("Nota final: $mediaFinal")
+}
